8c4942eb3959181b938b85e0b073753234e7a0a9
Ali Rahimi <ali.rahimi@gandom.co>
2024-11-30 22:09:16 +0330

    create files

---
+++ iosReview/iosReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosReview
+//
+//  Created by Ali Rahimi on 30/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
