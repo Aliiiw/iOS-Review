25485f0255f10b16ed3888aedbdaf69977ed0a50
Ali Rahimi <ali.rahimi@gandom.co>
2024-12-01 17:02:08 +0330

    add changing image logic

--- iosReview/FirstApp/FirstApp/ViewController.swift
+++ iosReview/FirstApp/FirstApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: with control and drag drop add the outlet to VC
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    
+    // MARK: we need to connect object in story board into VC
+    
+    
+    // MARK: first function that will be execute
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Hello World")
+        
     }
-
+    
+    
+    // MARK: it should be drag into class
+    
+    @IBAction func changeButton(_ sender: Any) {
+        imageView.image = UIImage.pic1
+    }
+    
 
 }
 
