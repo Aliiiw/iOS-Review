44f0f4821f58020536ee8176ed492667a33ae7b5
Ali Rahimi <ali.rahimi@gandom.co>
2024-12-02 20:15:28 +0330

    swift

---
+++ iosReview/SwiftPlayGround.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+// variables and constants
+
+var str = "ali"
+
+let name = "ali"
+
+// name = "6" can not assign new value
+
+str = "ali rhi"
+
+
+let PI:Float = 3.14
+
+
+
+str.lowercased()
+str.uppercased()
+
+let myDouble = 4.0
+
+let myBool = true
+
+// when you assign the type you can not change it
+
+var myString:String
+
+myString = "test"
+
+
